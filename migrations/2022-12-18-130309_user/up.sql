f20f8ef723be2e430f04fd54a2cc38342cf4aff6
martin <martinbrunel@hotmail.fr>
2022-12-27 23:37:57 +0100

    save

--- migrations/2022-12-18-130309_user/up.sql
+++ migrations/2022-12-18-130309_user/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE user (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(256) NOT NULL,
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(100) NOT NULL,
+    password VARCHAR(256) NOT NULL
 );
