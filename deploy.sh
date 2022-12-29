9cc3f9ada6906a239820be180b98d0eed0a1b125
martin <martinbrunel@hotmail.fr>
2022-12-29 10:49:09 +0100

    save

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git pull;
 sudo systemctl stop rocket.service;
 /home/ubuntu/.cargo/bin/diesel migration run;
-cargo run --release;
+cargo build --release;
 sudo systemctl start rocket.service;
 exit 0;
